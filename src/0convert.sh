64d6ec8cc498730cba13d7758360f4d12f7f9099
Alexey Varfolomeev <varlesh@gmail.com>
2019-06-06 14:43:47 +0400

    remove old png's

--- src/0convert.sh
+++ src/0convert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-rm drawable/*.png
+rm -r drawable/*.png
 for file in *.svg; do rsvg-convert -h 192 -w 192 $file > drawable/$file.png; done
 rename s/.svg//g drawable/*.png
-rm ../app/src/main/res/drawable-nodpi/*.png
+rm -r ../app/src/main/res/drawable-nodpi/*.png
 cp drawable/*.png ../app/src/main/res/drawable-nodpi/
