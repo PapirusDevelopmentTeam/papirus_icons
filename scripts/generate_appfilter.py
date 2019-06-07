08141122b25dd57b2c7de7cae41eeea19a6379ed
Sergei Eremenko <finalitik@gmail.com>
2019-06-07 16:14:01 +0300

    generate_appfilter.py: read paths to db and appfilter files from env

--- scripts/generate_appfilter.py
+++ scripts/generate_appfilter.py
@@ -1,10 +1,11 @@
 import json
-from os import path, remove
+from os import environ, path, remove
 
 ABS_PATH = path.dirname(path.abspath(__file__))
-DB_FILE = path.join(ABS_PATH, 'data.json')
 
-APPFILTER_FILE = path.join(ABS_PATH, 'xml/appfilter.xml')
+DB_FILE = environ.get('DB_FILE', path.join(ABS_PATH, 'data.json'))
+APPFILTER_FILE = environ.get('APPFILTER_FILE', path.join(ABS_PATH,
+    'xml/appfilter.xml'))
 
 # Remove the existing appfilter.xml file
 if path.exists(APPFILTER_FILE):
