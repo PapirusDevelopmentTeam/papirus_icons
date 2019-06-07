413164c769d86a2d1eef0d300ddc2ade0d7d5f01
Sergei Eremenko <finalitik@gmail.com>
2019-06-07 17:59:09 +0300

    Remove 0convert.sh script

--- src/0convert.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm -r drawable/*.png
-for file in *.svg; do rsvg-convert -h 192 -w 192 $file > drawable/$file.png; done
-rename s/.svg//g drawable/*.png
-rm -r ../app/src/main/res/drawable-nodpi/*.png
-cp drawable/*.png ../app/src/main/res/drawable-nodpi/
