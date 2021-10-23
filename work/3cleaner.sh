898d5e2bda1d043bf89d6c476471b68c3e3cd2d0
Alexey Varfolomeev <varlesh@gmail.com>
2021-10-23 08:56:48 +0400

    ask function on cleaner

--- work/3cleaner.sh
+++ work/3cleaner.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 # Delete all files
+echo -n "Delete all working files? (Y/n) "
+read item
+case "$item" in
+    y|Y) echo "Files deleted!"
+        ;;
+    n|N) echo "Canceled!"
+        exit 0
+        ;;
+    *) echo "Files deleted!"
+        ;;
+esac
 rm -rf icons/*.png
 rm -rf icons/*.webp
 rm list.txt output.txt failed.txt
