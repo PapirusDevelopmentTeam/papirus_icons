d06257ff34014d10e97cdd4fb5a04b2d903c6389
Alexey Varfolomeev <varlesh@gmail.com>
2021-10-23 06:47:38 +0400

    added checker

---
+++ work/2checker.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# List failed downloading apk's
+grep -vxFf output.txt list.txt >> failed.txt
+echo "This packages can not be downloaded from Google Play! Please check package name or this paid app."
+cat failed.txt
