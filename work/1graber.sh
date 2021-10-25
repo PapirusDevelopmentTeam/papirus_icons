657cad1ce45478ef2eef925b9affdb374191549c
Alexey Varfolomeev <varlesh@gmail.com>
2021-10-25 10:19:43 +0400

    Update 1graber.sh

--- work/1graber.sh
+++ work/1graber.sh
@@ -29,9 +29,9 @@ chmod +x icon.sh
 # get package label from apk
 aapt dump badging $PACKAGE.apk | grep -oP "(?<=application-label:')[^*]+" | awk '{print tolower($0)}' | sed "s/'$//" | sed "s/\ /_/g" | sed "s/-/_/g" >> result.txt
 # get package name from apk
-aapt dump badging $PACKAGE.apk | sed -n "/package/ s/.*name=.\([a-zA-Z0-9.]\+\)..*/\1/p" >> result.txt
+aapt dump badging $PACKAGE.apk | sed -n "/package/ s/.*name=.\([a-zA-Z0-9._-]\+\)..*/\1/p" >> result.txt
 # get launch activity from apk
-aapt dump badging $PACKAGE.apk | sed -n "/launchable-activity/ s/.*name=.\([a-zA-Z0-9.]\+\)..*/\1/p" >> result.txt
+aapt dump badging $PACKAGE.apk | sed -n "/launchable-activity/ s/.*name=.\([a-zA-Z0-9._-]\+\)..*/\1/p" >> result.txt
 # add separator
 echo "*********" >> result.txt
 # clean
