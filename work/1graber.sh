1849769410995d9a478a45e2141363694b7f0cc8
Alexey Varfolomeev <varlesh@gmail.com>
2021-10-27 10:01:53 +0400

    simplification 1graber.sh

--- work/1graber.sh
+++ work/1graber.sh
@@ -11,11 +11,12 @@ do
 export PACKAGE="$entry"
 # download package from Google Play
 gplaydl download --packageId $PACKAGE
-sed -i s/PACKAGE/$PACKAGE/g scraper.py
-python3 scraper.py | sed "s/http/wget\ -O\ icons\/$PACKAGE\.png\ http/g" >> icon.sh
-sed -i s/$PACKAGE/PACKAGE/g scraper.py
-chmod +x icon.sh
-./icon.sh
+# set package name on scraper for download icon from Google Play
+sed -i "s/PACKAGE/$PACKAGE/g" scraper.py
+# download icon from Google Play
+wget --quiet -O icons/$PACKAGE.png $(python3 scraper.py)
+# restore scraper
+sed -i "s/$PACKAGE/PACKAGE/g" scraper.py
 # get package label from apk
 aapt dump badging $PACKAGE.apk | grep -oP "(?<=application-label:')[^*]+" | awk '{print tolower($0)}' | sed "s/'$//" | sed "s/\ /_/g" | sed "s/-/_/g" >> result.txt
 # get package name from apk
@@ -27,5 +28,4 @@ echo "*********" >> result.txt
 # clean
 rm -rf $PACKAGE
 rm -rf $PACKAGE.apk
-rm icon.sh
 done
